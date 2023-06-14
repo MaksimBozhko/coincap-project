568efe6dcad44424c976ba77d2b19a4946aa5a0f
MaksimBozhko <bochkomax2000@gmail.com>
2023-06-14 19:20:57 +0300

    add: wallet

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,18 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import s from "./Header.module.scss"
 import { Navbar } from "../../Navbar"
 import { PopularCoins } from "../../PopularCoins"
 import { StateCase } from "../../StateCase"
+import SuperButton from "../../../shared/components/superButton/SuperButton"
+import { AddModal } from "../../Table/ui/TableRow/modal/addModal/AddModal"
+import { WalletModal } from "./modal/walletModal/WalletModal"
 
 export const Header = () => {
+  const [value, setValue] = useState('')
+
+
+  const clickBtnHandler = () => {
+    setValue('WALLET')
+  }
+  const onBuyCoinsHandler = (count: number) => {
+    // setCoins({ count, name: value })
+  }
+  const onCloseModal = () => {
+    setValue('')
+  }
   return (
     <div className={s.header}>
       <div className={s.container}>
         <div>logo</div>
         <PopularCoins />
-        <Navbar />
+        <SuperButton className={s.btn} onClick={clickBtnHandler}>WALLET</SuperButton>
+        {/*<Navbar />*/}
         <StateCase />
       </div>
+      {!!value && (
+        <WalletModal name={value} callback={onBuyCoinsHandler} onClose={onCloseModal} />
+      )}
     </div>
   )
 }
---
+++ src/widgets/Header/ui/modal/walletModal/WalletModal.tsx
@@ -0,0 +1,72 @@
+import React, { FC, useState } from "react"
+import s from "./WalletModal.module.scss"
+import { ReactComponent as Close } from "app/img/close.svg"
+import classNames from "shared/lib/classNames/classNames"
+import { Modal } from "shared/modal/Modal"
+import SuperButton from "shared/components/superButton/SuperButton"
+import { Input } from "shared/components/Input/Input"
+import { useSelector } from "react-redux"
+import { RootState } from "../../../../../app/store/store"
+import { getPrice } from "../../../../Table/model/helpers/getPrice"
+
+type DeletePropsType = {
+  name: string
+  isOpen?: boolean
+  onClose?: () => void
+  className?: string
+  callback?: (count: number) => void
+}
+
+export const WalletModal: FC<DeletePropsType> = ({
+                                                name,
+                                                isOpen,
+                                                onClose,
+                                                className,
+                                                callback
+                                              }) => {
+  const items = useSelector((state: RootState) => state.coins.case)
+  const [valueInput, setValueInput] = useState("")
+
+  const onCloseHandler = () => {
+    if (onClose) {
+      onClose()
+    }
+  }
+  const onAddHandler = () => {
+    if (callback) {
+      callback(+valueInput)
+    }
+    if (onClose) {
+      onClose()
+    }
+  }
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      className={classNames("", {}, [className])}
+    >
+      <div className={s.titleBlock}>
+        <h5 className={s.title}>{name}</h5>
+        <div className={s.close} onClick={onCloseHandler}><Close /></div>
+      </div>
+      <div>
+        {items.map((coin) => (
+          <div className={s.coin}>
+            <div>
+              <p className={s.name}>{coin.name}</p>
+              <p className={s.rank}>{coin.symbol}</p>
+            </div>
+            <div className={s.info}>
+              <p>{getPrice((+coin.priceUsd * coin.count).toString())}</p>
+              <p>{Number(coin.changePercent24Hr).toFixed(2)}</p>
+            </div>
+            <div>
+              -
+            </div>
+          </div>
+        ))}
+      </div>
+    </Modal>
+  )
+}

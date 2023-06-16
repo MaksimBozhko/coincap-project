ea7a937669307c93f36f3d2160e1933077b1115a
MaksimBozhko <bochkomax2000@gmail.com>
2023-06-16 11:15:32 +0300

    delete: basename

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { store } from "./app/store"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-  <BrowserRouter basename={'coincap-project'}>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>

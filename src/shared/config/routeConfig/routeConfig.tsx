92f6d986b08390e8b80a55dcc4527bbfa4991382
MaksimBozhko <bochkomax2000@gmail.com>
2023-06-17 18:34:24 +0300

    fix: 1

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -8,17 +8,17 @@ export enum AppRoutes {
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: "",
+  [AppRoutes.MAIN]: "/",
   [AppRoutes.ABOUT]: "/:id",
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
+  [AppRoutes.ABOUT]: {
+    path: RoutePath.about,
+    element: <AboutPage />
+  },
   [AppRoutes.MAIN]: {
     path: RoutePath.main,
     element: <MainPage />
   },
-  [AppRoutes.ABOUT]: {
-    path: RoutePath.about,
-    element: <AboutPage />
-  }
 }

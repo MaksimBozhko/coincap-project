fed0cc3d12b4dee496d33174e3291ba937a33b3f
MaksimBozhko <bochkomax2000@gmail.com>
2023-06-17 17:55:11 +0300

    fix: add HashRouter

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,18 +1,14 @@
 import React, { Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
 import { routeConfig } from "shared/config/routeConfig/routeConfig"
-import { MainPage } from "../../../../pages/MainPage"
-import { AboutPage } from "../../../../pages/AboutPage"
 
 export const AppRouter = () => {
   return (
     <Suspense fallback={<>Loading...</>}>
       <Routes>
-        {/*{Object.values(routeConfig).map(({ element, path }) => (*/}
-        {/*  <Route key={path} path={path} element={element} />*/}
-        {/*))}*/}
-        <Route path={'/'} element={<MainPage/>}/>
-        <Route path={'/:id'} element={<AboutPage/>}/>
+        {Object.values(routeConfig).map(({ element, path }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
       </Routes>
     </Suspense>
   )
